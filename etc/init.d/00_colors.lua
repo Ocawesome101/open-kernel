669e52f32428044c9f1bf51420c2b9551718c36c
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2020-01-28 21:12:38 -0500

    Delete 00_colors.lua

--- etc/init.d/00_colors.lua
+++
@@ -1,8 +0,0 @@
--- Colors --
-kernel.log("Loading colors API")
-local ok, err = loadfile("/lib/colors.lua")
-if not ok then
-  kernel.log("Error " .. err .. "while loading /lib/colors.lua")
-  return
-end
-ok()
