1c621e38efe28ec8d97577ce1adaf83df4fc5b23
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2020-02-17 20:40:26 -0500

    add table.copy

--- etc/init.d/40_tables.lua
+++ etc/init.d/40_tables.lua
@@ -5,3 +5,11 @@ function table.new(items)
   setmetatable(returnTable, {__index=table}) --Why this isn't just done by default for all tables I have no idea.
   return returnTable
 end
+
+function table.copy(tbl)
+  local rtn = {}
+  for k,v in pairs(tbl) do
+    rtn[k] = v
+  end
+  return rtn
+end
